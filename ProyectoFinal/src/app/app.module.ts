b280c9a21bb16180d42f19d6e415db9eaf5981e7
FranciscaJerezC <56319323+FranciscaJerezC@users.noreply.github.com>
2022-05-08 18:04:37 -0400

    components
    components folders

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
+import { ListComponent } from './componente/list/list.component';
+import { NavbarComponent } from './componente/navbar/navbar.component';
+import { TitleComponent } from './componente/title/title.component';
+import { ScreensComponent } from './screens/screens.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { ListScreenComponent } from './screens/list-screen/list-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent,
+    ListComponent,
+    NavbarComponent,
+    TitleComponent,
+    ScreensComponent,
+    ServiciosComponent,
+    HomeScreenComponent,
+    ListScreenComponent
   ],
   imports: [
     BrowserModule,
