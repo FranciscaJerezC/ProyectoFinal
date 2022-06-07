59cc3b571c0e63640c38f04259c0fe62f472ab64
FranciscaJerezC <56319323+FranciscaJerezC@users.noreply.github.com>
2022-06-06 21:22:54 -0400

    json listo

--- ProyectoFinal/src/app/components/login/login.component.ts
+++ ProyectoFinal/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public loginForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email:[''],
+      password:['']
+
+    })
+  }
+  login(){
+    this.http.get<any>("http://localhost:3000/registerUsuarios")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user){
+        alert("Ingreso Correcto");
+        this.loginForm.reset();
+        this.router.navigate(['home'])
+      }else{
+        alert("Usuario no encontrado");
+      }
+
+    },err=>{
+      alert("Algo salió mal")
+    })
+    
   }
 
 }
--- ProyectoFinal/src/app/components/register/register.component.ts
+++ ProyectoFinal/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder} from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registerForm!: FormGroup;
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      email:[''],
+      password:['']
+
+    })
+  }
+  register(){
+    this.http.post<any>("http://localhost:3000/registerUsuarios",this.registerForm.value)
+    .subscribe(res=>{
+      alert("Registro Correcto");
+      this.registerForm.reset();
+      this.router.navigate(['login']);
+    },err=>{
+      alert("Error al registrarse")
+    })
+    
   }
 
 }
